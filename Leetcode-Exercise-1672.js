0ddc3612723ecc5b57c187a159449d6be9b2d0b1
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-05-26 14:00:01 -0500

    Add files via upload

---
+++ Leetcode-Exercise-1672.js
@@ -0,0 +1,20 @@
+
+  function maximumWealth([]) {
+  let maxCustomerWealth = 0;
+
+  for (let i = 0; i < accounts.length; i++) {//Loop through each customer (or item) of the array (0,1,2).
+    let currentCustomerWealth = 0;
+
+    for (let j = 0; j < accounts[i].length; j++) {//This is another loop — this one goes through the bank accounts for the current customer.
+      currentCustomerWealth += accounts[i][j];//Accounts[i] is the current customer (a row). Accounts[i][j] is the money they have in one bank account.
+    }
+
+    if (currentCustomerWealth > maxCustomerWealth) {
+      maxCustomerWealth = currentCustomerWealth;
+    }
+  }
+
+  return maxCustomerWealth;
+}
+
+const accounts = [[7,1,3], [2,8,7], [1,9,5]];
