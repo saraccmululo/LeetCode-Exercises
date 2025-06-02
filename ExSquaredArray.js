ef994a81c9a34a53f35fb90d608fe383e1185df9
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-06-02 12:54:45 -0500

    completed exercise Squared Array

--- ExSquaredArray.js
+++ ExSquaredArray.js
@@ -1,6 +1,18 @@
+//Using for loop version:
 var sortedSquares = function(nums) {
-    
+  for(let i=0; i<nums.length; i++) {
+    nums[i]= nums[i]**2;
+  }
+  return nums.sort((a,b)=> a-b);
 };
+
+nums=[-4,-1,0,3,10];
+console.log(sortedSquares(nums));
+
+//Using .map() version:
+var sortedSquares = function(nums) {
+  return nums.map(num=>num**2).sort((a,b)=>a-b);
+}
 
 nums=[-4,-1,0,3,10];
 console.log(sortedSquares(nums));
