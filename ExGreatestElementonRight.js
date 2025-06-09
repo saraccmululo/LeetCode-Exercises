d90dc81f7e534edbd1b6343a397c0dd996a04a1d
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-06-09 11:59:50 -0500

    completed exercise greatest element on the right

---
+++ ExGreatestElementonRight.js
@@ -0,0 +1,33 @@
+//Nested loop solution:
+function replaceElements(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let max = arr[i + 1];
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] > max) {
+        max = arr[j];
+      }
+    }
+    arr[i] = max;
+  }
+  arr[arr.length - 1] = -1;
+  return arr;
+}
+
+arr = [17,18,5,4,6,1];
+console.log(replaceElements(arr));
+
+//Rserve loop optimized solution:
+function replaceElements(arr) {
+  let max = -1; // Last element becomes -1
+  for (let i = arr.length - 1; i >= 0; i--) {
+    let current = arr[i]; // Save current value before overwriting
+    arr[i] = max;         // Replace current with the greatest on its right
+    if (current > max) {
+      max = current;      // Update max for the next iteration
+    }
+  }
+  return arr;
+}
+
+arr = [17,18,5,4,6,1];
+console.log(replaceElements(arr));
