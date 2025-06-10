5057a916db7d08d0ebaa6382ce948a40b2c1551c
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-06-10 12:17:36 -0500

    new solution for remove duplicate array

--- ExRemoveDuplicatesArray.js
+++ ExRemoveDuplicatesArray.js
@@ -1,5 +1,6 @@
 //Solution using .splice():
 var removeDuplicates = function(nums) {
+  if (nums.length === 0) return [];
     let i=1;
     while(i<nums.length){
       if(nums[i]===nums[i-1]) {
@@ -12,10 +13,22 @@ var removeDuplicates = function(nums) {
     return k;
 };
 
+//similar result as above:
+ let i=0;
+    while(i<nums.length-1){
+      if(nums[i]===nums[i+1]) {
+        nums.splice(i+1,1);
+      }else{
+        i++
+      }
+    }
+    const k= nums.length;
+    return k;
+
 //Solution using two pointers(more efficient):
 var removeDuplicates = function(nums) {
   if(nums.length===0) return 0;
-  let k=1;
+  let k=1; //K is the counter. K starts with 1 because the first number is always unique.
   for(let i=1; i<nums.length; i++){
     if (nums[i]!==nums[i-1]) { //Unique — copy to nums[k], Duplicate — do nothing.
       nums[k] ==nums[i];
@@ -27,3 +40,10 @@ var removeDuplicates = function(nums) {
 
 nums = [1, 1, 2, 3, 3, 4];
 console.log(removeDuplicates(nums));
+
+//Solution creating a new array (take extra space):
+const uniqueSet=new Set(nums);
+const setArray=Array.from(uniqueSet);
+return setArray.length
+
+
