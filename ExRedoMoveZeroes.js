80ff1168a4a4b73350d0975ca926ff35d5c599e7
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-07-09 12:16:53 -0500

    redid Move Zeroes exercise

---
+++ ExRedoMoveZeroes.js
@@ -0,0 +1,18 @@
+var moveZeroes = function(nums){
+  let k=0;
+  for(i=0; i<nums.length; i++){
+    if(nums[i]!==0){
+      nums[k]=nums[i];
+      k++;
+  }
+  }
+  console.log(k)
+  for(i=k; i<nums.length; i++){
+    nums[i]=0;
+  }
+  return nums
+}
+const nums = [0,1,0,3,12]
+const num = [0]
+console.log(moveZeroes(nums))//Output:[1,3,12,0,0]
+console.log(moveZeroes(num))//Output: [0]
