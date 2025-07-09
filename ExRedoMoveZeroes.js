4268a74e5947c933ebb36352289ea32ef88b5957
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-07-09 12:17:30 -0500

    Update ExRedoMoveZeroes.js

--- ExRedoMoveZeroes.js
+++ ExRedoMoveZeroes.js
@@ -1,13 +1,13 @@
 var moveZeroes = function(nums){
   let k=0;
-  for(i=0; i<nums.length; i++){
+  for(let i=0; i<nums.length; i++){
     if(nums[i]!==0){
       nums[k]=nums[i];
       k++;
   }
   }
   console.log(k)
-  for(i=k; i<nums.length; i++){
+  for(let i=k; i<nums.length; i++){
     nums[i]=0;
   }
   return nums
